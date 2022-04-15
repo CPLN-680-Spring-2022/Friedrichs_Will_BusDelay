7495f2470c055ea6eba188769110f235692b9707
Willfriedrichs <will.friedrichs@gmail.com>
2022-04-15 07:31:33 -0400

    added 4-13 data

--- code/dashboard_code/dashboard_code.R
+++ code/dashboard_code/dashboard_code.R
@@ -22,7 +22,7 @@ numpoints = st_as_sf(numpoints,  coords = c("Lon", "Lat"))
 
 numpoints$StopId<-as.character(numpoints$StopId)
 
-early_04_11 = read.csv("https://raw.githubusercontent.com/CPLN-680-Spring-2022/Friedrichs_Will_BusDelay/main/raw_data/map_data/early_04_10r.csv")
+early_04_11 = read.csv("https://raw.githubusercontent.com/CPLN-680-Spring-2022/Friedrichs_Will_BusDelay/main/raw_data/map_data/early_04_11.csv")
 colnames(early_04_11) = c("tripID", "route", "x1","StopId","x2","timestamp","x3")
 early_04_11 = early_04_11 %>% na.omit() %>% select(c("tripID", "route", "StopId", "timestamp"))
 early_04_11 = early_04_11[!early_04_11$StopId == "null", ] 
@@ -31,12 +31,30 @@ early_04_11join = merge(numpoints, early_04_11, "StopId")
 
 
 
-cancel_04_11 = read.csv("https://raw.githubusercontent.com/CPLN-680-Spring-2022/Friedrichs_Will_BusDelay/main/raw_data/map_data/cancel_04_10r.csv")
+cancel_04_11 = read.csv("https://raw.githubusercontent.com/CPLN-680-Spring-2022/Friedrichs_Will_BusDelay/main/raw_data/map_data/cancel_04_11.csv")
 colnames(cancel_04_11) = c("tripID", "route", "x1","StopId","x2","timestamp","x3")
 cancel_04_11 = cancel_04_11 %>% na.omit() %>% select(c("tripID", "route", "StopId", "timestamp"))
 cancel_04_11 = cancel_04_11[!cancel_04_11$StopId == "null", ] 
 
 cancel_04_11join = merge(numpoints, cancel_04_11, "StopId")
+
+
+
+early_04_13 = read.csv("https://raw.githubusercontent.com/CPLN-680-Spring-2022/Friedrichs_Will_BusDelay/main/raw_data/map_data/early_04_13.csv")
+colnames(early_04_13) = c("tripID", "route", "x1","StopId","x2","timestamp","x3")
+early_04_13 = early_04_13 %>% na.omit() %>% select(c("tripID", "route", "StopId", "timestamp"))
+early_04_13 = early_04_13[!early_04_13$StopId == "null", ] 
+
+early_04_13join = merge(numpoints, early_04_13, "StopId")
+
+
+
+cancel_04_13 = read.csv("https://raw.githubusercontent.com/CPLN-680-Spring-2022/Friedrichs_Will_BusDelay/main/raw_data/map_data/cancel_04_13.csv")
+colnames(cancel_04_13) = c("tripID", "route", "x1","StopId","x2","timestamp","x3")
+cancel_04_13 = cancel_04_13 %>% na.omit() %>% select(c("tripID", "route", "StopId", "timestamp"))
+cancel_04_13 = cancel_04_13[!cancel_04_13$StopId == "null", ] 
+
+cancel_04_13join = merge(numpoints, cancel_04_13, "StopId")
 
 # early_04_11 = read.csv("early_04_11.csv")
 # cancel_04_11 = read.csv("cancel_04_11.csv")
@@ -68,34 +86,48 @@ checkmarkcode = "here"
 
 xabbr = routes$lineabbr
 
+percent1 = q
+percent2 = q
+percent3 = q
+
+percentlogged = q
+
+route1 = q
+route2 = q
+route3 = q
+
+routelogged = q
+
 ui <- navbarPage("SEPTA Bus Reliability Tracker",
   tabPanel("Summary Statistics",
     sidebarLayout(
       sidebarPanel(
         fluidRow(
           column(width=4, 
-                 selectInput(inputId = "baboo", label = "", choices = c("Since","On  ")),
+                 selectInput(inputId = "since_on", label = "", choices = c("Since","On  ")),
                  selected = "plot"),
           column(width=6, 
-                 selectInput(inputId = "baboo2", label = "", choices = c("March 11, 2015","September 29, 2042")),
+                 selectInput(inputId = "date", label = "", choices = c("April 13, 2022",
+                                                                         "April 12, 2022",
+                                                                         "April 11, 2022")),
                  selected = "plot")
         )
       ),
       mainPanel(
         style = "font-size:20px;",
-        strong("Since October 21, 2015:\n "),
-        p(span("30%", style = "color:blue"),
+        #strong("Since October 21, 2015:\n "),
+        p(span("NA%", style = "color:blue"),
           "of scheduled SEPTA stops have been \"ghosted\"."),
-        p(span("30%", style = "color:blue"),
+        p(span("NA%", style = "color:blue"),
           "of scheduled SEPTA stops have been 5 or more minutes early."),
-        p(span("30%", style = "color:blue"),
+        p(span("NA%", style = "color:blue"),
           "of scheduled SEPTA stops have been 10 or more minutes late."),
         p("\n"),
-        p(span("Route XX", style = "color:blue"),
+        p(span("Route NA", style = "color:blue"),
           "is the route with the most \"ghosted\" stops."),
-        p(span("Route XX", style = "color:blue"),
+        p(span("Route nA", style = "color:blue"),
           "is the route most frequently 5 or more minutes early."),
-        p(span("Route XX", style = "color:blue"),
+        p(span("Route NA", style = "color:blue"),
           "is the route most frequently 10 or more minutes late.")
       )
     )
@@ -105,10 +137,12 @@ ui <- navbarPage("SEPTA Bus Reliability Tracker",
       sidebarPanel(
         fluidRow(
           column(width=4, 
-                 selectInput(inputId = "since_on", label = "", choices = c("Since","On  ")),
+                 selectInput(inputId = "since_on2", label = "", choices = c("Since","On  ")),
                  selected = "plot"),
           column(width=6, 
-                 selectInput(inputId = "date", label = "", choices = c("March 11, 2015","September 29, 2042")),
+                 selectInput(inputId = "date2", label = "", choices = c("April 13, 2022",
+                                                                        "April 12, 2022",
+                                                                        "April 11, 2022")),
                  selected = "plot")
         ),
         fluidRow(
@@ -156,7 +190,7 @@ ui <- navbarPage("SEPTA Bus Reliability Tracker",
         fluidRow(column(width=12, leafletOutput("mymap"))),
         fluidRow(
         column(width=4, 
-               selectInput(inputId = "SPANOO",label = "tdoo", choices = c("s","bap")),
+               selectInput(inputId = "SPANOO",label = "SEPTA Bus Network in Greater Philadelphia Region", choices = c("Early Arrivals","Cancellations")),
                selected = "plot")
         )
       )
@@ -185,16 +219,68 @@ server <- function(input, output, session) {
     }
   })
   map_data <- reactive({
-    #if input$"Date"
-    trend_data = s
-    if (input$"SPANOO" == "bap") {
-      trend_data = bap
+    point_data = s
+    if (input$"date" == "April 11, 2022") {
+      if (input$"SPANOO" == "Early Arrivals") {
+        point_data = early_04_11join
+      }
+      else {
+        point_data = cancel_04_11join
+      }
     }
     else {
-      trend_data = s
+      if (input$"SPANOO" == "Early Arrivals") {
+        point_data = early_04_13join
+      }
+      else {
+        point_data = cancel_04_13join
+      }
     }
-    return(trend_data)
+    return(point_data)
   })
+  percentlogged_ghosted <- reactive({
+    return("NA")
+  })
+  percentlogged_early <- reactive({
+    return("NA")
+  })
+  percentlogged_late <- reactive({
+    return("NA")
+  })
+  most_ghosted <- reactive({
+    return("NA")
+  })
+  most_early <- reactive({
+    return("NA")
+  })
+  most_late <- reactive({
+    return("NA")
+  })
+  
+  # if (input$"since_on" == "On  "){
+  #   if (input$"date" == "April 11, 2022"){
+  #     routelogged = "23"
+  #   }
+  #   if (input$"date" == "April 12, 2022"){
+  #     routelogged = "19"
+  #   }
+  #   else {
+  #     routelogged = "5"
+  #   }
+  # }
+  # if (input$"since_on" == "Since"){
+  #   if (input$"date" == "April 11, 2022"){
+  #     routelogged = "A"
+  #   }
+  #   if (input$"date" == "April 12, 2022"){
+  #     routelogged = "B"
+  #   }
+  #   else {
+  #     routelogged = "C"
+  #   }
+  # }
+  # 
+  # 
   output$mymap <- renderLeaflet({
     # The leaflet function creates the zoomable image. 
     leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
@@ -203,7 +289,8 @@ server <- function(input, output, session) {
       # Set the view to the coordinates of Philadelphia
       setView(lng = -75.1635, lat = 39.9528, zoom = 10) %>%
       # addCircleMarkers displays the point data.
-      addPolylines(data = map_data(), color = 'red', weight = 2,
+      addPolygons(data = point_data()) %>%
+      addPolylines(data = routes, color = 'white', weight = 2,
                    label = ~paste("Here be county ", lineabbr), # note the tilde / ~
                    labelOptions = labelOptions(noHide = F, direction = "top"))
   })
